b87a0c3806fad87345a27ac9c8a15a1de7643834
ThomasvanderVeen <thomas_vdveen@live.nl>
2024-04-02 19:30:53 +0200

    -

--- general.py
+++ general.py
@@ -16,7 +16,7 @@ parameters = {'N_SIMULATIONS': 2,
 constants = {'N_ANGLES': 18,
              'N_JOINTS': 3,
              'T_TOTAL': 5,
-             'dt': 0.001,
+             'dt': 0.00025,
              }
 
 constants['N_STEPS'] = int(constants['T_TOTAL']/constants['dt'])
@@ -27,7 +27,7 @@ custom_linestyles = ['-', '--', '-.', ':', '-', '--', ':', '-.']
 
 plot_parameters = {'legend.fontsize': 10,
                    'figure.figsize': (5.728, 3.54),
-                   'figure.dpi': 100,
+                   'figure.dpi': 300,
                    'axes.labelsize': 15,
                    'axes.titlesize': 15,
                    'xtick.labelsize': 10,
@@ -35,3 +35,86 @@ plot_parameters = {'legend.fontsize': 10,
                    }
 
 pylab.rcParams.update(plot_parameters)
+
+def safe_divide(numerator, denominator):
+    if isinstance(numerator, np.ndarray):
+        result = np.zeros_like(numerator, dtype=float)
+
+        # Check for zero values in the denominator
+        zero_indices = np.where(denominator == 0)
+
+        # Set the corresponding elements in the result to zero for zero denominators
+        result[zero_indices] = 0.0
+
+        # Perform division for non-zero denominators
+        non_zero_indices = np.where(denominator != 0)
+        result[non_zero_indices] = numerator[non_zero_indices] / denominator[non_zero_indices]
+    else:
+        if denominator == 0:
+            return 0
+        else:
+            return numerator/denominator
+
+    return result
+
+
+def get_firing_rate_ISI(spike_train, dt):
+    spike_index = np.where(spike_train == 1)[0]
+
+    if len(spike_index) > 1:
+        inter_spike_interval = np.diff(spike_index) * dt
+        firing_rate = 1 / inter_spike_interval
+        firing_rate = np.append(firing_rate, firing_rate[-1])
+
+        firing_rate = np.concatenate([[0], firing_rate, [0]])
+        spike_index = np.concatenate([[0], spike_index, [spike_train.size - 3]])
+    else:
+        firing_rate = np.array([])
+
+    return firing_rate, spike_index
+
+
+def get_firing_rate_convolve(spike_train, dt, t=0.5, sigma=3, nan_bool=True):
+    n = int(t / dt)
+
+    firing_rate = np.convolve(spike_train.astype(int), np.ones(n), mode='same') / t
+    firing_rate = gaussian_filter1d(firing_rate, sigma=sigma)
+
+    if nan_bool:
+        firing_rate[firing_rate < 0.000001] = np.nan
+
+    return firing_rate
+
+
+def get_confusion_matrix(test_array, real_array):
+    intersect = test_array + real_array
+    difference = test_array - real_array
+
+    true_positive = intersect[intersect > 1.5].size
+    false_positive = difference[difference > 0.5].size
+    true_negative = intersect[intersect < 0.5].size
+    false_negative = difference[difference < -0.5].size
+
+    return true_positive, false_positive, true_negative, false_negative
+
+
+def matthews_correlation(true_positive, true_negative, false_positive, false_negative):
+    numerator = (true_positive * true_negative) - (false_positive * false_negative)
+    denominator = ((true_positive + false_positive) * (true_positive + false_negative) * (true_negative + false_positive) * (true_negative + false_negative)) ** 0.5
+
+    MCC = safe_divide(numerator, denominator)
+    return MCC
+
+
+def get_statistics(true_positive, true_negative, false_positive, false_negative):
+
+    P = true_positive + false_negative
+    N = false_positive + true_negative
+    T = P+N
+
+    MCC = matthews_correlation(true_positive, true_negative, false_positive, false_negative)
+    ACC = (true_positive + true_negative) / T
+    TPR = true_positive / (true_positive + false_negative)
+    TNR = true_negative / (true_negative + false_positive)
+
+    return [true_positive, true_negative, false_positive, false_negative, P, N, T, MCC, ACC, TPR, TNR]
